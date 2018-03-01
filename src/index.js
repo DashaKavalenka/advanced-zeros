01ab4a1714a61e5b2f6f2a5da4c7fb597d3970ab
DashaKavalenka <mymailDasha@gmail.com>
2018-03-01 08:27:18 +0300

    Implemented the main function; added a new one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+module.exports = {
+    getZerosCount: function (number, base) {
+        var k,
+            zeroCount,
+            powerOfEachMultiplier = [],
+            multipliers = [2, 3, 5, 7],
+            countOfMultiplier,
+            multipliersCount = new Map(),
+            countOfCurrentMultiplier,
+            baseMultipliers;
+
+        //находим множители base
+        baseMultipliers = this.computeMultipliers(base, multipliers, []);
+
+        //считаем, сколько каждый множитель встречается в полученном массиве множителей base
+        for (var i = 0; i < multipliers.length; i++) {
+            countOfMultiplier = 0;
+
+            for (var j = 0; j < baseMultipliers.length; j++) {
+                if (multipliers[i] === baseMultipliers[j]) {
+                    countOfMultiplier++;
+                }
+            }
+
+            if (countOfMultiplier !== 0) {
+                multipliersCount.set(multipliers[i], countOfMultiplier);
+            }
+        }
+
+        //вычисляем силу каждого множителя с учетом его количества
+        for (var i = 0; i < baseMultipliers.length; i++) {
+            zeroCount = 0;
+            k = 1;
+
+            while (Math.pow(baseMultipliers[i], k) <= number) {
+                zeroCount = zeroCount + parseInt(number / Math.pow(baseMultipliers[i], k));
+                k++;
+            }
+
+            countOfCurrentMultiplier = multipliersCount.get(baseMultipliers[i]);
+            if (countOfCurrentMultiplier >= 2) {
+                zeroCount = parseInt(zeroCount/countOfCurrentMultiplier);
+            }
+
+            powerOfEachMultiplier.push(zeroCount);
+        }
+
+        //сортируем и возвращаем наименьшую силу
+        return powerOfEachMultiplier.sort((a, b) => a-b)[0];
+    },
+
+    computeMultipliers: function (base, arrayOfBaseMultipliers, baseMultipliers) {
+        var initialValue = base,
+            remainder;
+
+        for (var i = 0; i < arrayOfBaseMultipliers.length; i++) {
+            remainder = base % arrayOfBaseMultipliers[i];
+            if (remainder === 0) {
+                baseMultipliers.push(arrayOfBaseMultipliers[i]);
+                base = base / arrayOfBaseMultipliers[i];
+                return this.computeMultipliers(base, arrayOfBaseMultipliers, baseMultipliers);//рекурсия
+            }
+        }
+
+        if (initialValue === base && initialValue !== 1) {
+            baseMultipliers.push(initialValue);
+        }
+
+        return baseMultipliers;
+    }
+};
